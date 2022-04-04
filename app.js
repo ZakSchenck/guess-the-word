e29f82e61f017301fa80a746b8d65e2037756ac2
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-04-04 17:39:39 -0400

    Added comments for readability

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const randomIndex = Math.floor(Math.random() * 5);
 const wordLength = document.getElementById("word-length");
 const hint = document.getElementById("hint");
 let livesRemaining = 5;
+// Information for the word as well as the description hint
 const words = [
   ["strawberry", "A fruit with 200 seeds"],
   ["coffee", "I can't go to work without it."],
@@ -14,20 +15,26 @@ const words = [
   ["ghostface", "Scream"],
   ["leatherface", "Texas Chainsaw Massacre"],
 ];
+// Gets random word from words array
 const randomWord = words[randomIndex];
 let answerArray = randomWord[0].split("");
 
+// Game state logic
 hint.innerText = `Your hint is: "${randomWord[1]}"`;
 wordLength.innerText = `Your word has ${randomWord[0].length} letters`;
+
 let letters = answerArray.map((letter) => "__ ");
 wordText.innerText = letters.join("");
 
 guessBtn.addEventListener("click", (e) => {
   let guess = guessInput.value;
+  // Logic that tells whether guess is correct
   if (randomWord[0].includes(guess)) {
     gameState.innerText = "You guessed correctly!";
+    // Logic that manipulates the array
     let newArray = answerArray.map((letter) => guess.includes(letter));
     let stringify = newArray.toString();
+    // Replaces string text
     let replace = stringify
       .replace(/false/gi, "__ ")
       .replace(/true/gi, guess)
@@ -35,6 +42,7 @@ guessBtn.addEventListener("click", (e) => {
     console.log(replace);
     wordText.innerText = replace.toUpperCase();
     guess = "";
+    // Incorrect guess logic
   } else {
     livesRemaining--;
     gameState.innerText = `You guessed incorrectly. You have ${livesRemaining} lives left...`;
