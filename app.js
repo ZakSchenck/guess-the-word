7668564c6ba104ab18c93908dec664b3f540905a
ZakSchenck <91504668+ZakSchenck@users.noreply.github.com>
2022-04-04 20:25:35 -0400

    Cut down code by replacing logic with a for loop

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const guessBtn = document.getElementById("guess-btn");
 const randomIndex = Math.floor(Math.random() * 5);
 const wordLength = document.getElementById("word-length");
 const hint = document.getElementById("hint");
+let g = []
 let state;
 let livesRemaining = 5;
 // Information for the word as well as the description hint
@@ -29,19 +30,19 @@ wordText.innerText = letters.join("");
 
 guessBtn.addEventListener("click", (e) => {
   // Logic that tells whether guess is correct
-  let guess = guessInput.value
+  let guess = guessInput.value;
   if (randomWord[0].includes(guess)) {
     gameState.innerText = "You guessed correctly!";
-     // Logic that manipulates the array
-  let newArray = answerArray.map((letter) => guess.includes(letter));
-  let stringify = newArray.toString();
-  // Replaces string text
-  let replace = stringify
-    .replace(/false/gi, "__ ")
-    .replace(/true/gi, guess)
-    .replace(/,/g, "");
-  console.log(replace);
-  wordText.innerText = replace.toUpperCase();
+    // Logic that manipulates the array
+    let newArray = answerArray.map((letter) => guess.includes(letter));
+    for (let i = 0; i < newArray.length; i++) {
+      if (newArray[i] === true) {
+        newArray[i] = guessInput.value;
+      } else {
+          newArray[i] = '_';
+      }
+    }
+    console.log(newArray)
   } else {
     // Incorrect guess logic
     livesRemaining--;
